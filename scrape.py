d32dd50650544ddec042fc627bab6bfd585831e8
Alex Jean-Baptiste <alxjeanbaptiste@gmail.com>
2017-10-03 21:56:45 -0400

    finished challenge

--- scrape.py
+++ scrape.py
@@ -1,1 +1,35 @@
+import sys
+import requests
+import json
 
+search_term = sys.argv[1]
+response = requests.get('http://api.duckduckgo.com/?q='+search_term+'&format=json')
+result_count = 0
+results = []
+topics = response.json()['RelatedTopics']
+i = 0
+while i <= 2:
+    results.append({ "desc": topics[i]['Text'], "url": topics[i]['FirstURL']})
+    i += 1
+    result_count += 1
+
+print i
+
+# while i < len(topics):
+#     print topics[i]['Topics'][0]
+#     i += 1
+# len(topics[i]['Topics'])
+while i < len(topics):
+    j = 0
+    while j < len(topics[i]['Topics']):
+        results.append({ "desc": topics[i]['Topics'][j]['Text'], "url": topics[i]['Topics'][j]['FirstURL']})
+        j += 1
+        result_count += 1
+    i += 1
+
+return_json = {}
+return_json['search_term'] = search_term
+return_json['result_count'] = result_count
+return_json['results'] = results
+
+print return_json
