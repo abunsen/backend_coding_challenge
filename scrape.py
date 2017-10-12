bbc1aebaa60c15c52065593c70354f2d49e08b02
Alex Jean-Baptiste <alxjeanbaptiste@gmail.com>
2017-10-12 17:45:21 -0400

    first solution

--- scrape.py
+++ scrape.py
@@ -1,35 +1,29 @@
 import sys
 import requests
 import json
+import bs4
 
 search_term = sys.argv[1]
-response = requests.get('http://api.duckduckgo.com/?q='+search_term+'&format=json')
-result_count = 0
-results = []
-topics = response.json()['RelatedTopics']
+response = requests.get('https://duckduckgo.com/html/?q=' + search_term)
+soup = bs4.BeautifulSoup(response.text, "html.parser")
+# print soup
+result = {}
+results_array = []
+# counter for number of results
 i = 0
-while i <= 2:
-    results.append({ "desc": topics[i]['Text'], "url": topics[i]['FirstURL']})
+
+# #getting the urls for each of the result pages
+for url in soup.select(".result__url"):
+    print url.get_text()
+    #
+    #
+    # gettting title of result page
+for title in soup.find_all('h2'):
+    print(title.get_text())
     i += 1
-    result_count += 1
-
-print i
-
-# while i < len(topics):
-#     print topics[i]['Topics'][0]
-#     i += 1
-# len(topics[i]['Topics'])
-while i < len(topics):
-    j = 0
-    while j < len(topics[i]['Topics']):
-        results.append({ "desc": topics[i]['Topics'][j]['Text'], "url": topics[i]['Topics'][j]['FirstURL']})
-        j += 1
-        result_count += 1
-    i += 1
-
-return_json = {}
-return_json['search_term'] = search_term
-return_json['result_count'] = result_count
-return_json['results'] = results
-
-print return_json
+# #
+# # #printing number of results
+# print 'Nuber of results: ' + str(i)
+result['search_term'] = search_term
+result['result_count'] = i
+print result
